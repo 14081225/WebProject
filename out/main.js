963bbee95beb0d723dd1ea99a20450371a44edd5
mxw <1563672996@qq.com>
2016-09-27 18:11:05 +0800

    提交图片

--- out/main.js
+++ out/main.js
@@ -5,6 +5,32 @@ var Greeter = (function () {
         this.span = document.createElement('span');
         this.element.appendChild(this.span);
         this.span.innerText = new Date().toUTCString();
+        var i = 0;
+        var j = 0;
+        var n = Math.floor(Math.random() * 10 + 1);
+        var line = 0;
+        var a = "";
+        var b = "";
+        var c = " ";
+        var d = "*";
+        line = getLine(n);
+        Build(line);
+        function Build(n) {
+            for (i = 0; i < n; i++) {
+                for (j = 0; j < n - i; j++) {
+                    a = a + c;
+                }
+                for (j = 0; j < 2 * i + 1; j++) {
+                    b = b + d;
+                }
+                console.log(a + b);
+                a = "";
+                b = "";
+            }
+        }
+        function getLine(line) {
+            return line;
+        }
     }
     Greeter.prototype.start = function () {
         var _this = this;
--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,38 @@ class Greeter {
         this.span = document.createElement('span');
         this.element.appendChild(this.span);
         this.span.innerText = new Date().toUTCString();
+        var i:number = 0;
+var j:number = 0;
+var n:number = Math.floor(Math.random()*10+1);
+var line:number = 0;
+
+var a = "";
+var b = "";
+var c = " ";
+var d = "*";
+
+line = getLine(n);
+Build(line);
+
+function Build(n:number):void {
+    for(i = 0;i < n; i++) {
+       for(j = 0;j < n-i;j++) {
+           a = a + c;
+       }
+       for(j = 0;j < 2*i+1;j++) {
+           b = b + d;
+       }
+       console.log(a + b);
+       a = ""
+       b = ""
+    }
+
+}
+
+
+function getLine(line:number):number {
+    return line;
+}
     }
 
     start() {
